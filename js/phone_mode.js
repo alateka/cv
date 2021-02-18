f00a22636470a5b6573a3f50e550e60ec96433e7
ALBERTO PÉREZ FRUCTUOSO <alateka@vivaldi.net>
2021-02-18 13:53:01 +0100

    :)

--- js/phone_mode.js
+++ js/phone_mode.js
@@ -4,10 +4,9 @@ function phone_mode() {
  let screemSize = window.screen.availWidth;
 
   if (screemSize > 833) {
-
-    document.getElementById('header_nav-light').style.fontSize = '100%';
-    document.getElementById('img_avatar').style.display = 'block';
-   document.getElementById('home_img').style.display = 'block';
+  document.getElementById('social_footer').style.display = 'none';
+  document.getElementById('header_nav-light').style.fontSize = '100%';
+  document.getElementById('img_avatar').style.display = 'block';
    document.getElementsByTagName('body')[0].style.fontSize = '100%';
    document.getElementById('header_content-light').style.fontSize = '100%';
    document.getElementById('img_avatar').style.width = '39%';
@@ -15,17 +14,21 @@ function phone_mode() {
     e.style.width = '100%';
    }
    document.getElementById('header_img').style.height = '511px';
-   document.getElementById('social_bar-light').style.display = 'flex';
-   document.getElementById('license_img').style.width = '33%';
-   document.getElementById('contact_img').style.width = '33%';
+   if ( document.getElementById('dark-mode').innerText == 'Modo Oscuro' ) {
+    document.getElementById('social_bar-light').style.display = 'flex';
+   } else {
+    document.getElementById('social_bar-dark').style.display = 'flex';
+   }
+   document.getElementById('license_img').style.display = 'block';
+   document.getElementById('contact_img').style.display = 'block';
    document.getElementsByTagName('footer')[0].style.fontSize = '100%';
    document.getElementById('header_content-light').style.top = '9%';
 
   } else if ( screemSize < 833 ) {
 
+    document.getElementById('social_footer').style.display = 'block';
    document.getElementById('header_nav-light').style.fontSize = '71%';
    document.getElementById('img_avatar').style.display = 'none';
-   document.getElementById('home_img').style.display = 'none';
    document.getElementsByTagName('body')[0].style.fontSize = '71%';
    document.getElementById('header_content-light').style.fontSize = '55%';
    document.getElementById('img_avatar').style.width = '15%';
@@ -33,11 +36,14 @@ function phone_mode() {
     e.style.width = '79%';
    }
    document.getElementById('header_img').style.height = '255px';
-   document.getElementById('social_bar-light').style.display = 'none';
-   document.getElementById('license_img').style.width = '19%';
-   document.getElementById('contact_img').style.width = '19%';
+   if ( document.getElementById('dark-mode').innerText == 'Modo Oscuro' ) {
+    document.getElementById('social_bar-light').style.display = 'none';
+   } else {
+    document.getElementById('social_bar-dark').style.display = 'none';
+   }
+   document.getElementById('license_img').style.display = 'none';
+   document.getElementById('contact_img').style.display = 'none';
    document.getElementsByTagName('footer')[0].style.fontSize = '71%';
    document.getElementById('header_content-light').style.top = '19%';
-
   }
 }
